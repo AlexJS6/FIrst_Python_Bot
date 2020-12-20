ddc15a76ff6f2db4a2918a2c7cacf614f71c98d1
Alexander Spiesberger <alex.spiesberger@gmail.com>
2020-12-20 14:55:37 +0000

    Adding the  command that gets a quote from an api.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import discord
 import os
+import requests
+import json
 
 client = discord.Client() # connection to discord
+
+# returns random inspirational quote with author
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return (quote)
 
 @client.event # async library
 async def on_ready(): # when bot is ready do this
@@ -13,8 +22,9 @@ async def on_message(message): # does something when someone sends a message, no
             return
 
     # for our bot we gona say that every command is a message that starts with a dollar sign
-    if message.content.startswith('$hello'): 
-        await message.channel.send('Hello!')
+    if message.content.startswith('$inspire'): 
+        quote = get_quote()
+        await message.channel.send(quote)
 
 # by default repl.it is public so secret keys are visible -> use environment variables
 # line to run the bot:
